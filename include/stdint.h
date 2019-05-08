d35a031d998e4d068324e52b2e14c75136a6cf11
Furkan Türkal <furkan.turkal@hotmail.com>
2019-05-08 23:37:11 +0300

    new datatype

--- include/stdint.h
+++ include/stdint.h
@@ -24,5 +24,7 @@ typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 
+typedef unsigned int uint8_t __attribute__((__mode__(__QI__)));
+typedef unsigned int uint16_t __attribute__ ((__mode__ (__HI__)));
 
 #endif
